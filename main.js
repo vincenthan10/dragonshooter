5618c82122142c91096a9cce07290578c9e8c2cf
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-12-08 15:51:36 -0500

    Screen size now always adjusts

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
+import Dragon from "./dragon.js";
+
 const canvas = document.getElementById("gameCanvas")
+
 const ctx = canvas.getContext("2d");
+
+const dragon = new Dragon(0.1 * canvas.width, 0.3 * canvas.height);
 
 function update(deltaTime) {
     const now = performance.now();
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    dragon.x = 0.1 * canvas.width;
+    dragon.y = 0.3 * canvas.height;
 }
 
 function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "skyblue";
-    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    dragon.draw(ctx);
+    console.log(canvas.height);
 }
 
 let lastTimestamp = 0;
