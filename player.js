6780d2fe993772b9a30f6288b70ac5b245456918
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2025-12-21 18:18:06 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,3 +1,4 @@
+import Bullet from "./bullet.js";
 export default class Player {
     constructor(x, y) {
         this.x = x;
@@ -12,16 +13,21 @@ export default class Player {
         this.BASEIMGHEIGHT = 95;
         this.imageWidth = this.BASEIMGWIDTH;
         this.imageHeight = this.BASEIMGHEIGHT;
+
+        this.bullets = [];
     }
 
-    draw(ctx, mapWidth, mapHeight, baseWidth, baseHeight) {
+    draw(ctx, mapWidth, mapHeight) {
         ctx.save();
         ctx.drawImage(this.img, this.x * mapWidth, this.y * mapHeight, this.imageWidth, this.imageHeight);
+        this.bullets.forEach(b => b.draw(ctx, mapWidth, mapHeight));
         ctx.restore();
     }
 
     update(deltaTime, keysPressed, mapWidth, mapHeight, canvas, baseWidth, baseHeight) {
+        this.bullets.forEach(b => b.update(deltaTime, mapWidth, mapHeight, baseWidth, baseHeight));
         if (this.alive) {
+            console.log(this.speedY * (mapHeight / baseHeight));
             this.imageWidth = this.BASEIMGWIDTH * (mapWidth / baseWidth);
             this.imageHeight = this.BASEIMGHEIGHT * (mapHeight / baseHeight);
             let dx = 0;
@@ -30,6 +36,7 @@ export default class Player {
             if (keysPressed.has("s") || keysPressed.has("arrowdown")) dy += this.speedY * (mapHeight / baseHeight);
             if (keysPressed.has("a") || keysPressed.has("arrowleft")) dx -= this.speedX * (mapWidth / baseWidth);
             if (keysPressed.has("d") || keysPressed.has("arrowright")) dx += this.speedX * (mapWidth / baseWidth);
+
             if (dx !== 0 && dy !== 0) {
                 dx /= Math.sqrt(2);
                 dy /= Math.sqrt(2);
@@ -44,14 +51,17 @@ export default class Player {
                 dy = 0;
             }
             if (this.y + this.imageHeight / mapHeight >= 1.05 && dy > 0) {
-                console.log("down");
                 dy = 0;
             }
             let newX = this.x + dx;
             let newY = this.y + dy;
             this.x = newX;
             this.y = newY;
-            console.log(this.x + ", " + this.y);
+            // console.log(this.x + ", " + this.y);
         }
+    }
+
+    shoot() {
+        this.bullets.push(new Bullet(this.x, this.y, 0));
     }
 }
