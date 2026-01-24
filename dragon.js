b6efff1ff55c0243f6e36ed7a79232a5b47cbb60
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2026-01-23 20:25:46 -0500

    Dragon now constantly follows player

--- dragon.js
+++ dragon.js
@@ -2,6 +2,7 @@ export default class Dragon {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.speed = 0.1;
 
         this.img = new Image();
         this.img.src = "images/greendragon.png";
@@ -13,5 +14,17 @@ export default class Dragon {
         ctx.save();
         ctx.drawImage(this.img, this.x * mapWidth, this.y * mapHeight, this.BASEIMGWIDTH * (mapWidth / baseWidth), this.BASEIMGHEIGHT * (mapHeight / baseHeight));
         ctx.restore();
+    }
+
+    follow(target, deltaTime) {
+        const dx = target.x - this.x;
+        const dy = target.y - this.y;
+        const dist = Math.sqrt(dx * dx + dy * dy);
+        if (dist === 0) return;
+
+        let newX = this.x + (dx / dist) * this.speed * deltaTime / 1000;
+        let newY = this.y + (dy / dist) * this.speed * deltaTime / 1000;
+        this.x = newX;
+        this.y = newY;
     }
 }
