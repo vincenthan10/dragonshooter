45bfd056384eb2cce3a6823e63957a6dd909934d
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2026-02-02 20:39:17 -0500

    Fireball kills player now

--- fireball.js
+++ fireball.js
@@ -2,8 +2,9 @@ export default class Bullet {
     constructor(x, y, dir) {
         this.x = x;
         this.y = y;
-        this.baseSpeed = 0.005;
+        this.baseSpeed = 0.004;
         this.speed = this.baseSpeed * dir;
+        this.damage = 1;
 
         if (dir > 0) {
             this.img = new Image();
@@ -16,6 +17,8 @@ export default class Bullet {
         this.BASEIMGHEIGHT = 20;
         this.imageWidth = this.BASEIMGWIDTH;
         this.imageHeight = this.BASEIMGHEIGHT;
+        this.width = 0;
+        this.height = 0;
     }
 
     draw(ctx, mapWidth, mapHeight) {
@@ -27,6 +30,15 @@ export default class Bullet {
     update(deltaTime, mapWidth, mapHeight, baseWidth, baseHeight) {
         this.imageWidth = this.BASEIMGWIDTH * (mapWidth / baseWidth);
         this.imageHeight = this.BASEIMGHEIGHT * (mapHeight / baseHeight);
+        this.width = this.imageWidth / mapWidth;
+        this.height = this.imageHeight / mapHeight;
         this.x += this.speed;
+    }
+
+    isColliding(entity) {
+        if (this.x + this.width * 0.8 >= entity.x && this.x <= entity.x + entity.width * 0.9 && this.y + this.height * 0.85 >= entity.y && this.y <= entity.y + entity.height * 0.9) {
+            return true;
+        }
+        return false;
     }
 }
