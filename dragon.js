6f07cc1454860cf563707ab1a4cd8c1629308c6d
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2026-02-18 22:25:19 -0500

    Nerfed dragon overall, added phases, health bar

--- dragon.js
+++ dragon.js
@@ -3,10 +3,13 @@ export default class Dragon {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.speed = 0.115;
+        this.speed = 0.09;
         this.xSpeed = 0;
         this.ySpeed = 0;
         this.facing = 1; // - = left, + = right
+        this.maxHp = 25;
+        this.hp = this.maxHp;
+        this.phase = 1;
 
         this.img = new Image();
         this.img.src = "images/greendragon.png";
@@ -20,13 +23,13 @@ export default class Dragon {
         this.height = 0;
 
         this.charging = false;
-        this.restTime = Math.random() * 1500 + 3000;
+        this.restTime = Math.random() * 2000 + 4000;
         this.chargeTime = Math.random() * 750 + 2750;
         this.lastMoveTime = 0;
 
         this.fireballs = []
         this.shooting = true;
-        this.shootingDelay = 2400;
+        this.shootingDelay = 2500;
         this.lastShootTime = 0;
     }
 
@@ -38,6 +41,37 @@ export default class Dragon {
             ctx.drawImage(this.imgL, this.x * mapWidth, this.y * mapHeight, this.imageWidth, this.imageHeight);
         }
 
+        // HP bar
+        const barX = this.x * mapWidth;
+        const barY = (this.y - 0.02) * mapHeight;
+        const barWidth = this.imageWidth;
+        const barHeight = this.imageHeight / 15;
+
+        ctx.fillStyle = "red";
+        ctx.fillRect(barX, barY, barWidth, barHeight);
+
+        ctx.fillStyle = "limegreen";
+        ctx.fillRect(barX, barY, (this.hp / this.maxHp) * barWidth, barHeight);
+
+        if (this.phase == 1) {
+            ctx.strokeStyle = "green";
+        } else if (this.phase == 2) {
+            ctx.strokeStyle = "yellow";
+        } else {
+            ctx.strokeStyle = "red";
+        }
+        ctx.lineWidth = 2.5;
+        ctx.strokeRect(barX, barY, barWidth, barHeight);
+
+        const centerX = barX + barWidth / 2;
+        const centerY = barY + barHeight / 2;
+
+        ctx.font = "14px Arial";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "black";
+        ctx.fillText(this.hp + "/" + this.maxHp, centerX, centerY);
+
         this.fireballs.forEach(f => f.draw(ctx, mapWidth, mapHeight));
         ctx.restore();
     }
@@ -48,6 +82,31 @@ export default class Dragon {
         this.imageHeight = this.BASEIMGHEIGHT * (mapHeight / baseHeight);
         this.width = this.imageWidth / mapWidth;
         this.height = this.imageHeight / mapHeight;
+
+        if (this.hp >= this.maxHp * 0.6) {
+            this.phase = 1;
+        } else if (this.hp >= this.maxHp * 0.3 && this.hp < this.maxHp * 0.6) {
+            this.phase = 2;
+        } else {
+            this.phase = 3;
+        }
+
+        if (this.phase == 1) {
+            this.restTime = Math.random() * 2000 + 4000;
+            this.chargeTime = Math.random() * 750 + 2750;
+            this.shootingDelay = 2500;
+            this.speed = 0.09;
+        } else if (this.phase == 2) {
+            this.restTime = Math.random() * 1750 + 3250;
+            this.chargeTime = Math.random() * 1000 + 3000;
+            this.shootingDelay = 2200;
+            this.speed = 0.1;
+        } else {
+            this.restTime = Math.random() * 1000 + 2500;
+            this.chargeTime = Math.random() * 1500 + 3500;
+            this.shootingDelay = 1750;
+            this.speed = 0.115;
+        }
 
         this.fireballs.forEach(f => f.update(deltaTime, mapWidth, mapHeight, baseWidth, baseHeight));
         for (let i = this.fireballs.length - 1; i >= 0; i--) {
@@ -72,7 +131,7 @@ export default class Dragon {
                 this.facing = -1;
             }
             this.charging = true;
-            this.chargeTime = Math.random() * 1000 + 3000;
+            console.log(this.chargeTime);
             this.lastMoveTime = now;
         }
         if (this.charging) {
@@ -80,7 +139,7 @@ export default class Dragon {
             this.y += this.ySpeed * deltaTime / 1000;
             if (now - this.lastMoveTime >= this.chargeTime) {
                 this.charging = false;
-                this.restTime = Math.random() * 1750 + 2750;
+                console.log(this.restTime);
                 this.lastMoveTime = now;
             }
         }
