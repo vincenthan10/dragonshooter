31e6d21a1b237e131a54c72182186db94c404c26
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2026-04-26 16:24:49 -0400

    Lots of new effects

--- mysterybox.js
+++ mysterybox.js
@@ -44,7 +44,8 @@ export default class MysteryBox {
             if (this.activeTime >= this.despawnTime) {
                 this.active = false;
                 this.activeTime = 0;
-                this.despawnTime = Math.random() * 2500 + 5000;
+                this.inactiveTime = 0;
+                this.despawnTime = Math.random() * 2000 + 4000;
                 this.x = Math.random() * 0.9 + 0.05;
                 this.y = Math.random() * 0.7 + 0.25;
             }
@@ -52,8 +53,9 @@ export default class MysteryBox {
             this.inactiveTime += deltaTime;
             if (this.inactiveTime >= this.spawnTime) {
                 this.active = true;
+                this.activeTime = 0;
                 this.inactiveTime = 0;
-                this.spawnTime = Math.random() * 8000 + 9000;
+                this.spawnTime = Math.random() * 10000 + 10000;
             }
         }
         if (this.player.collected || this.dragon.collected) {
@@ -65,7 +67,7 @@ export default class MysteryBox {
                 } 
                 if (this.dragon.collected) {
                     this.dragon.collected = false;
-                    this.dragonEffect(this.dragon, false);
+                    this.dragonEffect(this.dragon, false, this.effectNumber);
                 }
                 this.affectTime = 0;
                 this.effectTime = Math.random() * 3000 + 4500;
@@ -82,19 +84,73 @@ export default class MysteryBox {
     }
 
     playerEffect(player, collected, effectNumber) {
-        let rolled = (effectNumber == 0) ? Math.floor(Math.random() * 2) + 1 : effectNumber;
+        let rolled = (effectNumber == 0) ? Math.floor(Math.random() * 8) + 1 : effectNumber;
         switch (rolled) {
             case 1:
-                player.speedMultiplier = collected ? 1.4 : 1;
+                player.speedMultiplier = collected ? 1.5 : 1;
                 break;
             case 2:
-                player.fireRateMultiplier = collected ? 0.6 : 1;
+                player.fireRateMultiplier = collected ? 0.65 : 1;
                 break;
+            case 3:
+                player.fireRateMultiplier = collected ? 1.2 : 1;
+                player.bulletDmg = collected ? 2 : 1;
+                break;
+            case 4:
+                player.speedMultiplier = collected ? 0.75 : 1;
+                player.fireRateMultiplier = collected ? 1.4 : 1;
+                player.sizeMultiplier = collected ? 1.3 : 1;
+                player.bulletDmg = collected ? 3 : 1;
+                break;
+            case 5:
+                player.speedMultiplier = collected ? 1.3 : 1;
+                player.fireRateMultiplier = collected ? 0.8 : 1;
+                player.sizeMultiplier = collected ? 0.65 : 1;
+                break;
+            case 6:
+                player.fireRateMultiplier = collected ? 1.8 : 1;
+                break;
+            case 7:
+                player.speedMultiplier = collected ? 0.6 : 1;
+                player.ltnInvinc = collected ? true : false;
+                break;
+            case 8:
+                player.speedMultiplier = collected ? 2 : 1;
+                player.fireRateMultiplier = collected ? 2 : 1;
+                break;
+
         }
         this.effectNumber = collected ? rolled : 0;
     }
 
-    dragonEffect(dragon, collected) {
-        dragon.speedMultiplier = collected ? 1.8 : 1;
+    dragonEffect(dragon, collected, effectNumber) {
+        let rolled = (effectNumber == 0) ? Math.floor(Math.random() * 5) + 1 : effectNumber;
+        switch (rolled) {
+            case 1:
+                dragon.speedMultiplier = collected ? 1.8 : 1;
+                break;
+            case 2:
+                dragon.speedMultiplier = collected ? 0.85 : 1;
+                dragon.sizeMultiplier = collected ? 1.2 : 1;
+                break;
+            case 3:
+                dragon.sizeMultiplier = collected ? 0.6 : 1;
+                dragon.speedMultiplier = collected ? 1.5 : 1;
+                dragon.moveMultiplier = collected ? 0.3 : 1;
+                break;
+            case 4:
+                dragon.speedMultiplier = collected ? 0.75 : 1;
+                dragon.fireRateMultiplier = collected ? 0.4 : 1;
+                dragon.moveMultiplier = collected ? 1.25 : 1;
+                dragon.ltnInvinc = collected ? true : false;
+                break;
+            case 5:
+                if (collected) {
+                    let heal = Math.round(Math.random() * 12 + 6);
+                    dragon.hp = Math.min(dragon.hp + heal, dragon.maxHp);
+                }
+                break;
+        }
+        this.effectNumber = collected ? rolled : 0;
     }
 }
