ddc70ee4a310637e08d2fc48296e01c3325785ab
vincenthan10 <113260293+vincenthan10@users.noreply.github.com>
2026-09-07 11:11:36 -0400

    Added homing type

--- fireball.js
+++ fireball.js
@@ -1,13 +1,15 @@
 export default class Fireball {
-    constructor(x, y, dir, damage, sizeMultiplier) {
+    constructor(x, y, dir, damage, sizeMultiplier, health, homing = false, target = null) {
         this.x = x;
         this.y = y;
         this.baseSpeed = 0.0065;
         this.dir = dir;
         this.speed = this.baseSpeed * this.dir;
         this.damage = damage;
-        this.health = 1;
+        this.health = health;
         this.sizeMultiplier = sizeMultiplier;
+        this.homing = homing;
+        this.target = target;
 
         if (dir > 0) {
             this.img = new Image();
@@ -22,11 +24,23 @@ export default class Fireball {
         this.imageHeight = this.BASEIMGHEIGHT * this.sizeMultiplier;
         this.width = 0;
         this.height = 0;
+        this.lastAdjustmentTime = 0;
+        this.adjustmentInterval = 750; // Update trajectory every 750ms
     }
 
     draw(ctx, mapWidth, mapHeight) {
         ctx.save();
-        ctx.drawImage(this.img, this.x * mapWidth, this.y * mapHeight, this.imageWidth, this.imageHeight);
+        const drawX = this.x * mapWidth;
+        const drawY = this.y * mapHeight;
+        if (this.homing && this.speedY !== undefined) {
+            const directionAngle = Math.atan2(this.speedY, this.speed);
+            const spriteAngle = this.dir > 0 ? 0 : Math.PI;
+            ctx.translate(drawX + this.imageWidth / 2, drawY + this.imageHeight / 2);
+            ctx.rotate(directionAngle - spriteAngle);
+            ctx.drawImage(this.img, -this.imageWidth / 2, -this.imageHeight / 2, this.imageWidth, this.imageHeight);
+        } else {
+            ctx.drawImage(this.img, drawX, drawY, this.imageWidth, this.imageHeight);
+        }
         ctx.restore();
     }
 
@@ -35,7 +49,40 @@ export default class Fireball {
         this.imageHeight = this.BASEIMGHEIGHT * (mapHeight / baseHeight) * this.sizeMultiplier;
         this.width = this.imageWidth / mapWidth;
         this.height = this.imageHeight / mapHeight;
+        // Handle homing bullet trajectory adjustment
+        if (this.homing && this.target && this.target.alive) {
+            this.lastAdjustmentTime += deltaTime;
+            
+            if (this.lastAdjustmentTime >= this.adjustmentInterval) {
+                // Calculate direction towards target
+                const targetCenterX = this.target.x + this.target.width / 2;
+                const targetCenterY = this.target.y + this.target.height / 3;
+                const bulletCenterX = this.x + this.width / 2;
+                const bulletCenterY = this.y + this.height / 3;
+                
+                const dx = targetCenterX - bulletCenterX;
+                const dy = targetCenterY - bulletCenterY;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                
+                if (distance > 0) {
+                    // Normalize and apply to speed components
+                    const normDx = dx / distance;
+                    const normDy = dy / distance;
+                    
+                    this.speed = this.baseSpeed * normDx;
+                    this.speedY = this.baseSpeed * normDy;
+                } else {
+                    this.speedY = 0;
+                }
+                
+                this.lastAdjustmentTime = 0;
+            }
+        }
+        
         this.x += this.speed;
+        if (this.homing && this.speedY !== undefined) {
+            this.y += this.speedY;
+        }
     }
 
     isColliding(entity) {
